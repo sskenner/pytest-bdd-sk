d00218703b48850aa5e9482e0979dd31786d2465
sk <sskenner@gmail.com>
2019-10-05 20:58:16 -0400

    parametrizing steps.

--- tests/features/cucumbers.feature
+++ tests/features/cucumbers.feature
@@ -7,3 +7,8 @@ Feature: Cucumber Basket
     Given the basket has "2" cucumbers
     When "4" cucumbers are added to the basket
     Then the basket contains "6" cucumbers
+  
+  Scenario: Remove cucumbers to a basket
+    Given the basket has "8" cucumbers
+    When "3" cucumbers are removed from the basket
+    Then the basket contains "5 " cucumbers
--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -7,6 +7,10 @@ from cucumbers import CucumberBasket
 def test_add():
   pass
 
+@scenario('../features/cucumbers.feature', 'Remove cucumbers to a basket')
+def test_remove():
+  pass
+
 @given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=dict(Number=int)))
 def basket(initial):
   return CucumberBasket(initial_count=initial)
@@ -16,6 +20,9 @@ def basket(initial):
 def add_cucumbers(basket, some):
   basket.add(some)
 
+@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=dict(Number=int)))
+def remove_cucumbers(basket, some):
+  basket.remove(some)
 
 @then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=dict(Number=int)))
 def basket_has_total(basket, total):
